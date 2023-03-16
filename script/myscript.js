11875038cc84488008ce44b17181b07b01eb0f7d
Wenwei007 <127804935+Wenwei007@users.noreply.github.com>
2023-03-16 00:43:02 +0000

    modify 4

--- script/myscript.js
+++ script/myscript.js
@@ -2,3 +2,9 @@
 setTimeout(() => {
     document.querySelector('.text').classList.add('reveal');
 }, 1000);
+setTimeout(() => {
+    document.querySelector('.text2').classList.add('reveal2');
+}, 1000);
+// window.onload = function () {
+//     document.body.style.opacity = "1";
+// }
