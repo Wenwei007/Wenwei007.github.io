219da80ef59f627ef7192bede18d08172401de8d
Wenwei007 <127804935+Wenwei007@users.noreply.github.com>
2023-03-17 01:59:04 +0000

    modified 7

--- script/myscript.js
+++ script/myscript.js
@@ -2,9 +2,22 @@
 setTimeout(() => {
     document.querySelector('.text').classList.add('reveal');
 }, 1000);
-setTimeout(() => {
-    document.querySelector('.text2').classList.add('reveal2');
-}, 1000);
+
 // window.onload = function () {
 //     document.body.style.opacity = "1";
 // }
+
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 400) {
+        $('.project').css('visibility', 'visible');
+        $('img').fadeIn(2000);
+    } else {
+        $('.project').css('visibility', 'hidden');
+        $('img').fadeOut(500)
+    }
+});
+
+// $(document).ready(function() {
+//     $('img').fadeIn(2000); // 2 seconds animation duration
+// });
